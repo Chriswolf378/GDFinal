1da102a1b68ee10b18b8c15f511d4382303cf6f4
Christian C <christian.colzier99@gmail.com>
2019-05-26 13:23:28 -0400

    Game Files
    All the game files from my pc to Git

---
+++ Christian Final/Assets/Scripts/Sign.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Sign : MonoBehaviour
+{
+    public GameObject dialogBox;
+    public Text dialogText;
+    public string dialog;
+    public bool playerInRange;
+    public string response;
+
+    void Start()
+    {
+        
+    }
+
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && playerInRange)
+        {    // If player is in range and inputting space
+            if (dialogBox.activeInHierarchy)
+            {    // If dialog box is on screen
+                if (dialogText.text != response)
+                {    // If dialogBox.text is not the response text
+                    dialogText.text = response;
+                }
+                else
+                {    // dialogBox.text is response text
+                    dialogBox.SetActive(false);
+                }
+            }
+            else
+            { // Dialog box was not on screen, so activate it
+                dialogBox.SetActive(true);
+                dialogText.text = dialog;
+            }
+        }
+    }
+
+    private     void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            playerInRange = true;
+        }
+
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            playerInRange = false;
+            dialogBox.SetActive(false);
+
+        }
+    }
+}
